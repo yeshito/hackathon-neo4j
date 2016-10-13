babf888a9c676e9641437c7d1d7d0d7101bc13f0
Yeshe Wingerd <yeshito@gmail.com>
2016-10-12 21:51:43 -0700

    most recent

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,35 @@
 'use strict'
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 const neo4j = require('neo4j-driver').v1;
-const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "washing1"));
+const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "washing"));
 const session = driver.session();
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-  session.run( "MATCH (c:Category) return c")
-    .then( result => {
-      console.log(JSON.stringify(result))
+router.get('/:category', function(req, res, next) {
+
+  session.run( "MATCH (c:Crime)-[:HAS_CATEGORY]-(:Category {name: {name} }) return c"), { name: req.params.category})
+    .then( results => {
+      let daysObj = {Monday: 1, Tuesday: 2, Wednesday: 3, Thursday: 4, Friday: 5, Saturday: 6, Sunday: 7}
+      let daysCount = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0};
+
+
+      results.records.forEach(crime => {
+        let crimeDay = crime['_fields'][0]['properties']['dayofweek'];
+        let crimeTime = crime['_fields'][0]['properties']['time'];
+        daysCount[daysObj[crimeDay]] += 1;
+      })
+      console.log(JSON.stringify(daysCount));
     })
     .catch(err => {
       console.log(err)
     })
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+//   res.send('index', { title: 'Express' });
+// });
+
+// module.exports = router;
